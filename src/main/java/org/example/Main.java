90efa5b70849e1a0731485c5765913f34aa1bde7
CRTXScyNet <108296204+CRTXScyNet@users.noreply.github.com>
2024-04-22 19:48:54 +0500

    Revert "HW-7 to check"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,106 +2,64 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Hello world!");
 
         // Task one
-        int bottle = 0;
-        int salary = 15000;
-        int goal = 2_459_000;
-        int months = 0;
-        while (bottle < goal) {
-            months++;
-            bottle = bottle + salary;
-            bottle = bottle + bottle / 100;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
-        System.out.println("Месяц " + months + ", сумма накоплений равна " + bottle + " рублей");
 
         // Task two
-        int i = 0;
-        while (i < 10) {
-            i++;
-            System.out.print(i + " ");
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i);
         }
-        System.out.println();
-        for (; i > 0; ) {
-            System.out.print(i + " ");
-            i--;
-        }
-        System.out.println();
 
         // Task three
-        int populationY = 12_000_000;
-        int birthsPerOneThousandCityzens = 17;
-        int deathsPerOneThousandCityzens = 8;
-        int birthsThisYear;
-        int deathsThisYear;
-        for (int j = 1; j <= 10; j++) {
-            birthsThisYear = populationY / 1000 * birthsPerOneThousandCityzens;
-            deathsThisYear = populationY / 1000 * deathsPerOneThousandCityzens;
-            populationY = populationY + birthsThisYear - deathsThisYear;
-            System.out.println("Год " + j + ", численность населения составляет " + populationY);
+        for (int i = 0; i <= 17; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
         }
 
         // Task four
-        double percent = 0.07f;
-        double deposite = 15000;
-        goal = 12_000_000;
-        int month = 0;
-        while (deposite < goal) {
-            month++;
-            deposite = deposite + (deposite * percent);
-            System.out.println("Месяц: " + month + ", сумма накоплений: " + (int) deposite);
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
         }
 
-        //Task five
-        deposite = 15000;
-        month = 0;
-        int halfYear = 6;
-        while (deposite < goal) {
-            month++;
-            deposite = deposite + (deposite * percent);
-            if (month % halfYear == 0) {
-                System.out.println("Месяц: " + month + ", сумма накоплений: " + (int) deposite);
-            }
+        // Task five
+        for (int year = 1904; year < 2096; year = year + 4) {
+            System.out.println(year + " год является високосным");
         }
 
         // Task six
-        deposite = 15000;
-        month = 0;
-        int monthsPerYear = 12;
-        int totalAmountOfYear = 9;
-        int distance = monthsPerYear * totalAmountOfYear;
-
-        while (month < distance) {
-            month++;
-            deposite = deposite + (deposite * percent);
-            if (month % halfYear == 0) {
-                System.out.println("Месяц: " + month + ", сумма накоплений: " + (int) deposite);
-            }
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
         }
 
-        // Task seven
-        int friday = 7;
-        int dayOfMonth = 1;
-        int daysInMonth = 30;
-        int amountOfDaysInWeek = 7;
-        while (dayOfMonth <= daysInMonth) {
-
-            if (dayOfMonth % amountOfDaysInWeek == friday || dayOfMonth % amountOfDaysInWeek == 0 && friday == amountOfDaysInWeek) {
-                System.out.println("Сегодня пятница, " + dayOfMonth + "-е число. Необходимо подготовить отчет");
-            }
-            dayOfMonth++;
+        //Task seven
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println(i);
         }
 
         // Task eight
-        int currentYear = 2024;
-        int start = currentYear - 200;
-        int period = 79;
-        int yearFromZero = 0;
-        do {
-            yearFromZero += period;
-            if (yearFromZero > start) {
-                System.out.println(yearFromZero);
-            }
-        } while (yearFromZero < currentYear);
+        int bottle = 0;
+        int salary = 29000;
+        for (int i = 0; i < 12; i++) {
+            bottle = bottle + salary;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + bottle + " рублей");
+        }
+
+        // Task nine
+        bottle = 0;
+        for (int i = 0; i < 12; i++) {
+            bottle = bottle + salary;
+            bottle = bottle + bottle / 100;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + bottle + " рублей");
+        }
+
+        //Task ten
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2*" + i + "=" + (2 * i));
+        }
     }
 }
